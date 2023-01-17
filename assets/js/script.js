f082510b4ab92acb46848b611a3011cfb32d51a7
raoakif <akifrao@gmail.com>
2023-01-18 03:12:01 +0500

    Add Navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,188 @@
-console.log("Hello World")
+// const projects = [
+//   {
+//     title: 'Project 1: Keeping track of hundreds of components',
+//     image:
+//       'https://raw.githubusercontent.com/RaoAkif/MicroversePortfolio/main/images/portfolio_modal/Portfolio_Projects%20-%20Mobile%20Modal_1.png',
+//     description:
+//       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release.",
+//     tags: {
+//       tag1: 'HTML',
+//       tag2: 'CSS',
+//       tag3: 'JS',
+//       tag4: 'React',
+//       tag5: 'Ruby',
+//     },
+//     linkLive: '#',
+//     linkSource: '#',
+//   },
+//   {
+//     title: 'Project 2: Keeping track of hundreds of components',
+//     image:
+//       'https://raw.githubusercontent.com/RaoAkif/MicroversePortfolio/main/images/portfolio_modal/Portfolio_Projects%20-%20Mobile%20Modal_2.png',
+//     description:
+//       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release.",
+//     tags: {
+//       tag1: 'HTML',
+//       tag2: 'CSS',
+//       tag3: 'JS',
+//       tag4: 'React',
+//       tag5: 'Ruby',
+//     },
+//     linkLive: '#',
+//     linkSource: '#',
+//   },
+//   {
+//     title: 'Project 3: Keeping track of hundreds of components',
+//     image:
+//       'https://raw.githubusercontent.com/RaoAkif/MicroversePortfolio/main/images/portfolio_modal/Portfolio_Projects%20-%20Mobile%20Modal_3.png',
+//     description:
+//       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release.",
+//     tags: {
+//       tag1: 'HTML',
+//       tag2: 'CSS',
+//       tag3: 'JS',
+//       tag4: 'React',
+//       tag5: 'Ruby',
+//     },
+//     linkLive: '#',
+//     linkSource: '#',
+//   },
+//   {
+//     title: 'Project 4: Keeping track of hundreds of components',
+//     image:
+//       'https://raw.githubusercontent.com/RaoAkif/MicroversePortfolio/main/images/portfolio_modal/Portfolio_Projects%20-%20Mobile%20Modal_4.png',
+//     description:
+//       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release.",
+//     tags: {
+//       tag1: 'HTML',
+//       tag2: 'CSS',
+//       tag3: 'JS',
+//       tag4: 'React',
+//       tag5: 'Ruby',
+//     },
+//     linkLive: '#',
+//     linkSource: '#',
+//   },
+//   {
+//     title: 'Project 5: Keeping track of hundreds of components',
+//     image:
+//       'https://raw.githubusercontent.com/RaoAkif/MicroversePortfolio/main/images/portfolio_modal/Portfolio_Projects%20-%20Mobile%20Modal_5.png',
+//     description:
+//       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release.",
+//     tags: {
+//       tag1: 'HTML',
+//       tag2: 'CSS',
+//       tag3: 'JS',
+//       tag4: 'React',
+//       tag5: 'Ruby',
+//     },
+//     linkLive: '#',
+//     linkSource: '#',
+//   },
+//   {
+//     title: 'Project 6: Keeping track of hundreds of components',
+//     image:
+//       'https://raw.githubusercontent.com/RaoAkif/MicroversePortfolio/main/images/portfolio_modal/Portfolio_Projects%20-%20Mobile%20Modal_6.png',
+//     description:
+//       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release.",
+//     tags: {
+//       tag1: 'HTML',
+//       tag2: 'CSS',
+//       tag3: 'JS',
+//       tag4: 'React',
+//       tag5: 'Ruby',
+//     },
+//     linkLive: '#',
+//     linkSource: '#',
+//   },
+// ];
+
+// const Portfolios = document.querySelector('#portfolios');
+
+// projects.forEach((project, index) => {
+//   Portfolios.innerHTML += `
+//     <div class='portfolio'>
+//       <img class='portfolio-image' src=${
+//   project.image
+// } alt='portfolio_image_${index}' />
+//       <div class='portfolio-desc'>
+//       <h2 class='project-title'>${project.title}</h2>
+//       <ul class='tags'>
+//       <li class='tag'>${project.tags.tag1}</li>
+//       <li class='tag'>${project.tags.tag2}</li>
+//       <li class='tag'>${project.tags.tag3}</li>
+//       <li class='tag'>${project.tags.tag4}</li>
+//       <li class='tag'>${project.tags.tag5}</li>
+//       </ul>
+//       <button type='button' id='see-project-${
+//   index + 1
+// }' class='primary-button button-states see-project'>See Project</button>
+//     </div>
+// </div>
+// `;
+// });
+
+
+
+
+
+
+
+
+
+
+// <div class="portfolio-single1 modal fade" id="portfolio-single1" tabindex="-1" role="dialog"
+//         aria-labelledby="portfolioModalScrollable" aria-hidden="true">
+//         <div class="modal-dialog modal-dialog-scrollable" role="document">
+//             <div class="modal-content text-muted">
+//                 <div class="modal-header" style="align-items: center;" >
+//                     <h2 class="modal-title pl-1" id="portfolioModalScrollable">Microverse <span class="base-color">Portfolio</span></h2>
+//                     <button style="margin-right: -12px;" type="button" class="close" data-dismiss="modal" aria-label="Close">
+//                         <span aria-hidden="true">&times;</span>
+//                     </button>
+//                 </div>
+//                 <div class="modal-body">
+//                     <div class="pt-4 pb-5">
+//                         <div class="container">
+//                             <div class="row">
+//                                 <div class="col-12 col-md-10 mx-auto">
+//                                     <div class="mt-5">
+//                                         <div class="portfolio-single-item image-border">
+//                                             <img src="assets/img/portfolio-02.png" class="d-block" alt="...">
+//                                         </div>
+//                                     </div>
+//                                     <div class="text-md-left mt-5">
+//                                         <p class="my-3 text-justify">This is a portfolio website that was build as a bootcamp project, using HTML, CSS, JavaScript, Bootstrap, Flexbox and Grid layout etc. It contains a Bio section, Technology Stack section, and the Recent Projects section, along with a contact form to reach out to the developer. This Portfolio Website maps the projects Data, its Screeshots and other stuff on to the DOM with the help of JavaScript Selectors. The Project section and Technologies can be updated just by updating the object variable file. </p>
+//                                     </div>
+//                                     <div class="row contact-info mt-5 align">
+//                                         <div class="col-md-12 mt-3 mt-md-0">
+//                                             <div class="text-center">
+//                                                 <div>
+//                                                     <p class="mb-0">HTML | CSS | JavaScript | Bootstrap | FontAwesome | FlexBox | Grid Layout</p>
+//                                                 </div>
+//                                                 <div>
+//                                                     <a href="https://raoakif.github.io/MicroversePortfolio/" target="_blank">
+//                                                         <button type="submit" class="mt-4 mr-4 py-2 px-3 button-border pill-button">
+//                                                             See Live
+//                                                             <img class='link-icons pl-2' src='./assets/img/icons/see-live.svg' alt=''>
+//                                                         </button>
+//                                                     </a>
+//                                                     <a href="https://github.com/RaoAkif/MicroversePortfolio" target="_blank">
+//                                                         <button type="submit" class="mt-4 mr-4 py-2 px-3 button-border pill-button">
+//                                                             See Source
+//                                                             <img class='link-icons  pl-2' src='./assets/img/icons/see-source.svg' alt=''>
+//                                                         </button>
+//                                                     </a>
+//                                                 </div>
+//                                             </div>
+//                                         </div>
+//                                         </div>
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
